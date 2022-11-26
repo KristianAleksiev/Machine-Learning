f3661192467e31c5418ed1baf0d2bb45c43d7322
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-11-26 18:25:10 +0200

    Add Machine Learning introduction

---
+++ 01.Introduction_to_ML/ml_introduction.py
@@ -0,0 +1,34 @@
+"""
+1. The scientific method - overview
+
+2. Machine learning
+Basic concepts:
+- Computer learning from data
+
+Types:
+- Supervised learning - regression, classification
+- Unsupervised learning - the program finds patterns in data - clustering analysis, dimensionality reduction
+- Reinforcement learning - Form of unsupervised, the program learns continuously - e.g. the program is playing a game
+
+Algorithms by task(models) overview:
+- Statistical algorithms
+- Regression - predicting a continuous variable
+- Classification - predicting class labels
+- Clustering - finding compact groups of data points
+- Dimensionality reduction - simplifying the input data
+- Optimization - minimize / maximize a target function
+- Testing and improvement algorithms - fine tuning and optimization of ML algos - meta-learning
+
+scikit-learn - ML models
+numpy, scipy, matplotlib
+
+3. Data management
+
+4. Machine learning process overview
+- Problem definition
+- Data analysis
+- Data preparation
+- Algo evaluation
+- Result improvements
+- Result presentation
+"""
