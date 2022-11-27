9b32b28dacdaa0c9d085aa102916124efa10cadc
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-11-27 17:39:33 +0200

    Add Linear and Logistic Regression

---
+++ 02.Linear_and_Logistic_Regression/linear_logistic_regressions.py
@@ -0,0 +1,24 @@
+"""
+1. Machine learning basics
+- Objective function
+- Cost function
+- Optimization
+
+2. Linear regression
+- Algorithm
+parameters of the model - they change during the process of learning (gradient descent coefficients)
+Optimal steps => optimal result => Greedy algorithms
+- Usage
+MSE, sqrt(MSE) - no gradient
+
+3. RANSAC
+- Randomized Sample Consensus - Removing the outliers
+- Testing a few models for outliers
+
+4. Polynomial regression
+- Precompute the columns
+
+5. Logistic regression
+- Algorithm- Predict one of several known classes, based on the input params
+- Usage
+"""
