e881356e8cd438d0cd3a05c0b50c8a9ce36f2e32
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-12-06 18:40:07 +0200

    Add Time Series

---
+++ 09.Time_Series/time_series.py
@@ -0,0 +1,6 @@
+"""
+ARMA/ARIMA models - Autoregressive, moving average models
+Markov chain - Random behavior model => Markov chain Monte Carlo
+Probabilistic graphical models
+Local regression - statsmodels STL
+"""
